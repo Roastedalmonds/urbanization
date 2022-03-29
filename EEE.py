d52a5a2b1c33ccd9340ed93d53a87978a6528639
Aditya Mahimkar <mahimkaradi@gmail.com>
2022-03-30 00:52:47 +0530

    Integrations

---
+++ EEE.py
@@ -0,0 +1,224 @@
+import imp
+import pandas as pd
+import geopandas as gpd
+import osmnx as ox
+from shapely import wkt
+import math
+from shapely.geometry import Point, Polygon, MultiPolygon, LineString
+
+def minmax(point, min, max):
+    return [(point[0] - min[0]) / (max[0] - min[0]), (point[1] - min[1]) / (max[1] - min[1])]
+
+def get_poly_coords(df, min_coord, max_coord):
+    df['geometry'] = df['geometry'].apply(wkt.loads)
+    gdf = gpd.GeoDataFrame(df, crs='epsg:4326')
+    jsonPoly=[]
+    polyType=[]
+    for x in range(len(gdf)):
+        if type(gdf.loc[x,'geometry'])==Polygon:
+            try:
+                polys=[]
+                for i,j in list(zip(gdf.loc[x,'geometry'].boundary.xy[0],gdf.loc[x,'geometry'].boundary.xy[1])):
+                    temp99 = minmax((i, j), min_coord, max_coord)
+                    polys.append(dict({'x':temp99[0],'y':temp99[1]}))
+                polys.append(polys[0])
+                jsonPoly.append([polys])
+                polyType.append(3)
+            except:
+                jsonPoly.append('NA')
+                polyType.append('NA')
+
+        elif type(gdf.loc[x,'geometry'])==MultiPolygon:
+            try:
+                mulPoly=[]
+                for t in range(len(gdf.loc[x,'geometry'].geoms)):
+                    polys=[]
+                    for i,j in zip(gdf.loc[x,'geometry'].geoms[t].boundary.xy[0],gdf.loc[x,'geometry'].geoms[t].boundary.xy[1]):
+                        temp99 = minmax((i, j), min_coord, max_coord)
+                        polys.append(dict({'x':temp99[0],'y':temp99[1]}))
+                    polys.append(polys[0])
+                    mulPoly.append(polys)
+                jsonPoly.append(mulPoly)
+                polyType.append(4)
+            except:
+                jsonPoly.append('NA')
+                polyType.append('NA')
+
+        elif type(gdf.loc[x,'geometry'])==Point:
+            temp99 = minmax((gdf.loc[x,'geometry'].xy[0], gdf.loc[x,'geometry'].xy[1]), min_coord, max_coord)
+            jsonPoly.append([dict({'x':temp99[0],'y':temp99[1]})])
+            polyType.append(1)
+
+        elif type(gdf.loc[x,'geometry'])==LineString:
+            try:
+                for i,j in zip(gdf.loc[x,'geometry'].boundary.xy[0],gdf.loc[x,'geometry'].boundary.xy[1]):
+                    temp99 = minmax((i, j), min_coord, max_coord)
+                    polys.append(dict({'x':temp99[0],'y':temp99[1]}))
+                jsonPoly.append(polys)
+                polyType.append(2)
+            except:
+                jsonPoly.append('NA')
+                polyType.append('NA')
+
+        else:
+            jsonPoly.append('NA')
+            polyType.append('NA')
+            print(f'Geometry not recognised at index: {x}')
+    return jsonPoly,polyType
+
+def get_historic_json_op(jsonPoly,polyType,df):
+    jsonOP=[]
+    for x in range(len(jsonPoly)):
+        try:
+            name=df.loc[x,'name']
+        except:
+            name='NA'
+
+        try:
+            if 'historic' in df.columns:
+                if not math.isnan(df.loc[x,'historic']):
+                    typ= str(df.loc[x,'historic'])+'_Historic'
+                else:
+                    typ='Historic'
+            elif 'tourism' in df.columns:
+                if not math.isnan(df.loc[x,'tourism']):
+                    typ=str(df.loc[x,'tourism'])+'_Tourism'
+                else:
+                    typ='Tourism'
+
+        except:
+            typ='Historic_Tourism'
+
+        if jsonPoly[x]=='NA':
+            pass
+        else:
+            temp={
+                'name':name,
+                'type':typ,
+                'polygon':polyType[x],
+                'geometry':jsonPoly[x]
+            }
+            jsonOP.append(temp)
+    return jsonOP
+
+def get_building_json_op(jsonPoly,polyType,df):
+    jsonOP=[]
+    for x in range(len(jsonPoly)):
+        try:
+            name=df.loc[x,'name']
+        except:
+            name='NA'
+
+        try:
+            if 'building' in df.columns:
+                if not math.isnan(df.loc[x,'building']) and df.loc[x,'building']=='industrial':
+                    typ= 'Industrial'
+                elif not math.isnan(df.loc[x,'building']) and df.loc[x,'building']=='commercial':
+                    typ='Commercial'
+        except:
+            typ='Building'
+
+        if jsonPoly[x]=='NA':
+            pass
+        else:
+            temp={
+                'name':name,
+                'type':typ,
+                'polygon':polyType[x],
+                'geometry':jsonPoly[x]
+            }
+            jsonOP.append(temp)
+    return jsonOP
+
+def get_forest_json_op(jsonPoly,polyType,df):
+    jsonOP=[]
+    for x in range(len(jsonPoly)):
+        try:
+            name=df.loc[x,'name']
+        except:
+            name='NA'
+
+        try:    
+            if 'water' in df.columns: #or (not (math.isnan(df.loc[x,'water']))):
+                if df.loc[x,'water']=='pond' or df.loc[x,'water']=='reservoir' or df.loc[x,'water']=='lake' or df.loc[x,'water']=='river' or df.loc[x,'water']=='canal': 
+                    typ='Water_Body'
+            else:
+                typ='Forest'
+        except:
+            typ='Forest'
+
+        if jsonPoly[x]=='NA':
+            pass
+        else:
+            temp={
+                'name':name,
+                'type':typ,
+                'polygon':polyType[x],
+                'geometry':jsonPoly[x]
+            }
+            jsonOP.append(temp)
+    return jsonOP
+
+def latlongBounds(temp):    
+    maxLatVal = -1000
+    maxLongVal = -1000
+    minLatVal = 1000
+    minLongVal = 1000
+    for i in range(len(temp)):
+        if temp[i][0]>=maxLatVal:
+            maxLatVal = temp[i][0]
+            maxLat = i
+        if temp[i][1]>=maxLongVal:
+            maxLongVal = temp[i][1]
+            maxLong = i
+        if temp[i][0]<=minLatVal:
+            minLatVal = temp[i][0]
+            minLat = i
+        if temp[i][1]<=minLongVal:
+            minLongVal = temp[i][1]
+            minLong = i
+    bounds = [maxLatVal, maxLongVal, minLatVal, minLongVal]
+    diffLat = maxLatVal - minLatVal
+    diffLong = maxLongVal - minLongVal
+    return (bounds, diffLat, diffLong)
+
+def computeEEE(coordinates, min_coord, max_coord):
+    print('9. EEE started')
+    bounds, diffLat, diffLong = latlongBounds(coordinates)
+    historicTag={'historic': True, 'tourism':True, 'leisure':'nature_reserve'}
+    buildingTag = {'building':True}
+    forestTag = {'boundary':['forest','forest_compartment'],'natural':'water','water':True}
+
+    G1 = ox.geometries.geometries_from_bbox(bounds[1],bounds[3],bounds[0],bounds[2], historicTag)
+    G2 = ox.geometries.geometries_from_bbox(bounds[1],bounds[3],bounds[0],bounds[2], buildingTag)
+    G3 = ox.geometries.geometries_from_bbox(bounds[1],bounds[3],bounds[0],bounds[2], forestTag)
+
+    G1.to_csv('historicTag.csv')
+    G2.to_csv('buildingTag.csv')
+    G3.to_csv('forestTag.csv')
+
+    historic = pd.read_csv('historicTag.csv')
+    building = pd.read_csv('buildingTag.csv')
+    forest = pd.read_csv('forestTag.csv')
+
+    historic.geometry.dropna(inplace=True)
+    building.geometry.dropna(inplace=True)
+    forest.geometry.dropna(inplace=True)
+
+    remove=['hotel','apartment','hostel','guest_house']
+    reflist=historic.columns
+    for x in remove:
+        if x in reflist:
+            historic = historic[(historic['tourism']!=x)]
+
+    jsonPoly,polyType = get_poly_coords(historic, min_coord, max_coord)
+    historicJSON = get_historic_json_op(jsonPoly,polyType,historic)
+
+    jsonPoly,polyType = get_poly_coords(building, min_coord, max_coord)
+    buildingJSON = get_building_json_op(jsonPoly,polyType,building)
+
+    jsonPoly,polyType = get_poly_coords(forest, min_coord, max_coord)
+    forestJSON = get_forest_json_op(jsonPoly,polyType,forest)
+
+    print('10. EEE finished')
+    return historicJSON, buildingJSON, forestJSON
---
+++ gui_app.py
@@ -0,0 +1,263 @@
+import streamlit as st
+import streamlit.components.v1 as components
+import streamlit_folium
+import numpy as np
+import geopandas as gpd
+import folium
+import geemap.colormaps as cm
+import geemap.foliumap as gmf
+import geemap
+import bundle
+import pickle
+import ee
+import math
+import pandas as pd
+from shapely import wkt
+from shapely.ops import split
+from shapely.geometry import Polygon, Point, LineString, MultiPolygon
+# import georasters as gr
+from scipy import spatial
+import tagee
+import EEE
+import warnings
+import osmnx as ox
+import json
+import sklearn
+
+warnings.filterwarnings('ignore', category=DeprecationWarning)
+
+def uploaded_file_to_gdf(data):
+    import tempfile
+    import os
+    import uuid
+
+    _, file_extension = os.path.splitext(data.name)
+    file_id = str(uuid.uuid4())
+    file_path = os.path.join(tempfile.gettempdir(), f"{file_id}{file_extension}")
+
+    with open(file_path, "wb") as file:
+        file.write(data.getbuffer())
+
+    if file_path.lower().endswith(".kml"):
+        gpd.io.file.fiona.drvsupport.supported_drivers["KML"] = "rw"
+        gdf = gpd.read_file(file_path, driver="KML")
+    else:
+        gdf = gpd.read_file(file_path)
+
+    return gdf
+
+def _quadrat_cut_geometry(geometry, quadrat_width, min_num=3): 
+    # create n evenly spaced points between the min and max x and y bounds
+    west, south, east, north = geometry.bounds
+    x_num = math.ceil((east - west) / quadrat_width) + 1
+    y_num = math.ceil((north - south) / quadrat_width) + 1
+    x_points = np.linspace(west, east, num=max(x_num, min_num))
+    y_points = np.linspace(south, north, num=max(y_num, min_num))
+    # create a quadrat grid of lines at each of the evenly spaced points
+    vertical_lines = [LineString([(x, y_points[0]), (x, y_points[-1])]) for x in x_points]
+    horizont_lines = [LineString([(x_points[0], y), (x_points[-1], y)]) for y in y_points]
+    lines = vertical_lines + horizont_lines
+    # recursively split the geometry by each quadrat line
+    for line in lines:
+        geometry = MultiPolygon(split(geometry, line))
+    return geometry
+
+def get_bounding_box(coord):
+    maxLatVal = -1000
+    maxLongVal = -1000
+    minLatVal = 1000
+    minLongVal = 1000
+    for i in range(len(coord)):
+        if coord[i][0]>=maxLatVal:
+            maxLatVal = coord[i][0]
+            maxLat = i
+        if coord[i][1]>=maxLongVal:
+            maxLongVal = coord[i][1]
+            maxLong = i
+        if coord[i][0]<=minLatVal:
+            minLatVal = coord[i][0]
+            minLat = i
+        if coord[i][1]<=minLongVal:
+            minLongVal = coord[i][1]
+            minLong = i
+    bounds = [maxLatVal, maxLongVal, minLatVal, minLongVal]
+    diffLat = maxLatVal - minLatVal
+    diffLong = maxLongVal - minLongVal
+    return ([minLatVal, minLongVal], [maxLatVal, maxLongVal], diffLat, diffLong)
+
+def explode(indf):
+    geometriess = []
+    for x in range(len(indf.geoms)):
+        geometriess.append(indf[x])
+    return geometriess
+
+def TAGEE(coordinates):
+    srtm = ee.Image("USGS/SRTMGL1_003")
+    worldCover = ee.ImageCollection("ESA/WorldCover/v100").sort('system:time_start', False).limit(1, 'system:time_start', False) \
+                .first().clip(ee.Geometry.Polygon(coordinates))
+
+    cityPoly = Polygon(coordinates)
+    cutBlocks =  _quadrat_cut_geometry(cityPoly, 0.098, 15)
+    print('3. cutBlocks done')
+
+    geom = ee.FeatureCollection(ee.Feature(ee.Geometry.Polygon(coordinates)))
+    gaussianFilter = ee.Kernel.gaussian(radius=3, sigma=2, units='pixels', normalize=True)
+    srtmSmooth = srtm.convolve(gaussianFilter).resample("bilinear")
+    terrainMetrics = tagee.terrainAnalysis(srtmSmooth, geom.geometry())
+    superReducer = ee.Reducer.median().combine(ee.Reducer.minMax(), "", True)
+    reduction = terrainMetrics.reduceRegions(geom,superReducer)
+    print('4. TAGEE_city done')
+
+    cityTagee=[]
+    cityTagee = reduction.getInfo()
+    cityFeatures = list(cityTagee['features'][0]['properties'].keys())
+    cityFeaturesVal = []
+    cityFeatures2=[]
+    
+    for i in range(len(cityFeatures)):
+        cityFeaturesVal.append(cityTagee['features'][0]['properties'][cityFeatures[i]])
+        cityFeatures2.append(cityFeatures[i]+'_city')
+    df = pd.DataFrame(columns = cityFeatures+cityFeatures2)
+    
+    for x in range(len(cutBlocks.geoms)):
+        coords=[]
+        for i,j in zip(cutBlocks[x].boundary.xy[0],cutBlocks[x].boundary.xy[1]):
+            coords.append([i,j]) 
+        geom = ee.FeatureCollection(ee.Feature(ee.Geometry.Polygon(coords)))
+        reduction = terrainMetrics.reduceRegions(geom,superReducer)
+        blockTagee=[]
+        blockTagee = reduction.getInfo()
+        df = df.append(blockTagee['features'][0]['properties'], ignore_index=True)
+
+    for x in range(len(cityFeatures2)):
+        df[cityFeatures2[x]]=cityTagee['features'][0]['properties'][cityFeatures[x]]
+    datalist = explode(cutBlocks)
+    cityBlocks = gpd.GeoDataFrame(gpd.GeoSeries(datalist))
+    print('5. TAGEE_tile done')
+
+    cityId=[]
+    for z in range(len(cityBlocks)):
+        cityId.append('location_'+str(z))
+    cityBlocks['id'] = cityId
+    cityBlocks.columns=['geometry','id']
+    cityBlocks.set_index('id', inplace=True)
+    cityBlocks[['Prob_O', 'Prob_N', 'Prob_G', 'Prob_R']] =  None
+    cityBlocks['cls'] = 'Nopattern'
+    
+    geemap.ee_export_image(worldCover, filename='Cover.tif', scale=90, file_per_band=True)
+    wc = gr.from_file('Cover.Map.tif')
+    try:
+        worldCover = wc.to_geopandas()
+        tree = spatial.KDTree(list(zip(worldCover['x'],worldCover['y'])))
+    
+        cover0=[]
+        for name in cityBlocks.index.to_list():
+            centroid = cityBlocks.loc[name].geometry.centroid
+            point = (centroid.y, centroid.x)
+            t = tree.query([centroid.x,centroid.y])
+            cover0.append(worldCover.iloc[t[-1]]['value'])
+        cityBlocks['cover'] = cover0
+
+    except:
+        cover0=list(np.zeros(len(cityBlocks)))
+        cityBlocks['cover'] = cover0
+    print('6. worldcover done')
+
+    grid = getProb(cityBlocks)
+
+    kk = grid
+    kk = kk.reset_index().reset_index()
+    df = df.reset_index()
+    df = df.merge(kk,on='index')
+    file_name = 'location.csv'
+    df.to_csv(file_name)
+
+    print('7. data created and saved')
+    return df
+
+def minmax(point, min, max):
+    return [(point[0] - min[0]) / (max[0] - min[0]), (point[1] - min[1]) / (max[1] - min[1])]
+
+def get_area_polygon(polygon):
+    polygon = polygon.to_crs({'init': 'epsg:3857'})
+    return (polygon.area) / 10**6
+
+def predict_layout(dataset):  # GRON --> return json
+    model = pickle.load(open('rf_63_acc.pkl', 'rb'))
+    print('8. Predictions')
+
+# main code
+st.title('Urbanization')
+
+m = gmf.Map(basemap='HYBRID', plugin_Draw=True, draw_export=True,
+               locate_control=True, plugin_LatLngPopup=True)
+m.add_basemap('ROADMAP')
+
+row1col1, row1col2 = st.columns(2)
+
+with row1col1:
+    keyword = st.text_input('Search for a location:', '')
+
+with row1col2:
+    if keyword:
+        locations = gmf.geocode(keyword)
+        if locations is not None and len(locations) > 0:
+            str_locations = [str(g)[1:-1] for g in locations]
+            location = st.selectbox("Select a location:", str_locations)
+            loc_index = str_locations.index(location)
+            selected_loc = locations[loc_index]
+            lat, lng = selected_loc.lat, selected_loc.lng
+
+is_lat_lng = st.checkbox('Use Latitude-Longitude')
+
+if is_lat_lng:
+    row2col1, row2col2 = st.columns(2)
+
+    with row2col1:
+        lat = st.number_input(label='Enter Latitude')
+    with row2col2:
+        lng = st.number_input(label='Enter Longitude')
+
+try:
+    m.set_center(lng, lat, 12)
+    st.session_state["zoom_level"] = 12
+except:
+    pass
+
+data = st.file_uploader("Upload a GeoJSON file to use as an ROI.",
+                        type=["geojson", "kml", "zip"])
+
+if data:
+    gdf = uploaded_file_to_gdf(data)
+    print(get_area_polygon(gdf))
+    st.session_state["roi"] = geemap.geopandas_to_ee(gdf, geodesic=False)
+    m.add_gdf(gdf, "ROI")
+    coordinates = list(gdf['geometry'].exterior[0].coords)
+    # print('coord', coordinates)
+    print('1. Coordinates Done')
+
+    min_coord, max_coord, diffLat, diffLong = get_bounding_box(coordinates)
+    print('2. BBox Done')
+
+    # print(min_coord, max_coord)
+    coords_json = []
+    for coord in coordinates:
+        min_max = minmax(coord, min_coord, max_coord)
+        coords_json.append(dict({'x':min_max[0],'y':min_max[1]}))
+
+    with open("./Generator/src/ts/impl/city_loc.json", "w") as outfile:
+        json.dump(coords_json, outfile)
+
+    # data = TAGEE(coordinates)
+
+    centroids = predict_layout(data)
+
+    historic, buildings, forest = EEE.computeEEE(coordinates[:-1], min_coord, max_coord)
+    
+    coasts = {'historic': historic, 'buildings': buildings, 'forest': forest}
+
+    with open("./Generator/src/ts/impl/coasts.json", "w") as outfile:
+        json.dump(coasts, outfile)
+    
+
+m.to_streamlit(height=600)
